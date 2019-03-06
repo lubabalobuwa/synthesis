83beabd225050cb3694c88f76a8786e9c0f3af65
Lubabalo Buwa <G17B3155@ict.ru.ac.za>
2019-03-07 01:31:20 +0200

    Just finished with digits function

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,28 +1,65 @@
 ﻿module Synthesis
 
-let abelar _ =
-    failwith "Not implemented"
+let abelar = fun v -> (v>12) && (v<3097) && (v%2=0)
+failwith "Not implemented"
+    (*match v > 12 with 
+    | false -> false
+    | true -> 
+        match v <3097 with 
+        | false -> false 
+        | true -> 
+            match v%12=0 with
+            | false -> false
+            | true -> true
+failwith "Not implemented"
+*)
 
-let area _ _ =
-    failwith "Not implemented"
+let area = 
+    fun b -> 
+        fun h -> 
+            match b < 0.0 || h<0.0 with
+            | true -> failwith 
+            //| _ -> 0.5 * b * h
+failwith "Not implemented"
 
-let zollo _ =
-    failwith "Not implemented"
 
-let min _ _ =
-    failwith "Not implemented"
+let zollo v =
+    match v<0 with
+    | true -> v*(-1)
+    | false -> v*2
+failwith "Not implemented"
 
-let max _ _ =
-    failwith "Not implemented"
+let min a b = 
+    match a<b with
+    | true -> a
+    | false -> b
+failwith "Not implemented"
 
-let ofTime _ _ _ =
-    failwith "Not implemented"
+let max a b =
+    match a> b with
+    | true -> a
+    | false -> b
+failwith "Not implemented"
 
-let toTime _ =
-    failwith "Not implemented"
+let ofTime h m s = (h*3600) + (m*60) + s
+failwith "Not implemented"
 
-let digits _ =
-    failwith "Not implemented"
+let toTime = fun s -> 
+                match s<0 with
+                | true -> (0,0,0)
+                | false -> (s/3600, (s%3600)/60, s % 60)
+failwith "Not implemented"
+
+let digits d = 
+    let rec count n = 
+        match n <> 0 with 
+        | false -> 1
+        | _ ->
+            match n/10=0 with 
+            | true -> 1
+            | _ -> 1 + count (n/10)
+    count d
+failwith "Not implemented"
 
 let minmax _ =
     failwith "Not implemented"
