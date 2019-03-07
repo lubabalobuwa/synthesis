89364a6b62174c447eef9f7fa16324d901f89b7e
Lubabalo Buwa <G17B3155@ict.ru.ac.za>
2019-03-07 16:36:08 +0200

    Left with 4 functions

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,52 +1,35 @@
 ﻿module Synthesis
 
-let abelar = fun v -> (v>12) && (v<3097) && (v%2=0)
-failwith "Not implemented"
-    (*match v > 12 with 
-    | false -> false
-    | true -> 
-        match v <3097 with 
-        | false -> false 
-        | true -> 
-            match v%12=0 with
-            | false -> false
-            | true -> true
-failwith "Not implemented"
-*)
+let abelar v = 
+    (v>12) && (v<3097) && (v%2=0)
 
-let area b h =
+let area b h = 
     match b<0.0 || h<0.0 with
     | true -> failwith "ShouldFail"
     | _ -> 0.5 * b * h 
 
-failwith "Not implemented"
-
-let zollo v =
+let zollo v = 
     match v<0 with
     | true -> v*(-1)
     | false -> v*2
-failwith "Not implemented"
 
 let min a b = 
     match a<b with
     | true -> a
     | false -> b
-failwith "Not implemented"
 
-let max a b =
+let max a b = 
     match a> b with
     | true -> a
     | false -> b
-failwith "Not implemented"
 
-let ofTime h m s = (h*3600) + (m*60) + s
-failwith "Not implemented"
+let ofTime h m s = 
+    (h*3600) + (m*60) + s
 
 let toTime = fun s -> 
                 match s<0 with
                 | true -> (0,0,0)
                 | false -> (s/3600, (s%3600)/60, s % 60)
-failwith "Not implemented"
 
 let digits d = 
     let rec count n = 
@@ -57,53 +40,57 @@ let digits d =
             | true -> 1
             | _ -> 1 + count (n/10)
     count d
-failwith "Not implemented"
 
-let minmax year =
+let minmax num =  
     failwith "Not implemented"
 
-let isLeap year=
-    fun year ->
-        match year < 1582 with 
-        | true -> failwith "Failed"
-        | _ -> 
-            match (year%4=0) || (year%100=0) || (year%400=0) with 
-            | false -> false
-            | true -> true   
-failwith "Not implemented"
+let isLeap y = 
+    match y < 1582 with 
+    | true -> failwith "failed"
+    | _-> match (y%4=0) && (y%100<>0) with
+          | true -> true
+          | _ -> y% 400 =0
 
-let month mon = 
-    match (mon<1)||(mon >12) with
-    | true -> failwith "Failed"
-    | _ ->
-        match mon with
-        | 1 -> ("January",31)
-        | 2 -> ("February", 28)
-        | 3 -> ("March", 31)
-        | 4 -> ("April", 30)
-        | 5 -> ("May", 31)
-        | 6 -> ("June", 30)
-        | 7 -> ("July", 31)
-        | 8 -> ("August", 31)
-        | 9 -> ("September", 30)
-        | 10 -> ("October", 31)
-        | 11 -> ("November", 30)
-        | 12 -> ("December", 31)
-        
-failwith "Not implemented"
+let month = function
+     | 1 -> ("January",31)
+     | 2 -> ("February", 28)
+     | 3 -> ("March", 31)
+     | 4 -> ("April", 30)
+     | 5 -> ("May", 31)
+     | 6 -> ("June", 30)
+     | 7 -> ("July", 31)
+     | 8 -> ("August", 31)
+     | 9 -> ("September", 30)
+     | 10 -> ("October", 31)
+     | 11 -> ("November", 30)
+     | 12 -> ("December", 31)  
+     | _-> failwith "Not a month"
 
-let toBinary binary =
-    let rec result n = 
-        match n>0 with 
-        | false -> 0
-        | _-> 
-            match n/2=0 with
-            | true -> 0
-            | _-> n%2 + result (n/2)
-    result binary
-failwith "Not implemented"
+let toBinary num = 
+    match num < 0 with
+    | true -> failwith "Negative number"
+    | _-> match num =0 with 
+          | true -> "0"
+          | _-> let rec count n result =
+                    match n>0 with 
+                    | false -> result
+                    | _-> match n%2 with
+                          | 0 -> count (n/2) ("0" + result)  
+                          | _ -> count (n/2) ("1" + result) 
+                count num ""
 
-let bizFuzz _ =
+let bizFuzz num =
+(*
+    let three = 0
+    let five =0
+    let both = 0
+    match num < 1 with 
+    | true -> failwith "Negative number"
+    | _-> let rec divide n = 
+              match n = num with 
+              | true -> (three, five, both)
+              | _-> 
+    *)
     failwith "Not implemented"
 
 let monthDay _ _ =
